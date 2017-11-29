f19950bd32d51cea4522ade38f59e41a04c32ba6
lnanhkhoa <lnanhkhoa303@gmail.com>
2017-11-29 19:56:26 +0700

    qcf
    qwd

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// index.js
-// 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-var express = require('express');
 var app = express();
+
+
+
+app()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
 app.get('/', function (req, res) {
    res.send('Hello World');
@@ -11,11 +21,4 @@ app.get('/', function (req, res) {
 app.get('/api', function (req, res) {
    stepsCount = Math.random()*100 |0;
    res.send({results:stepsCount});
-})
-
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
 })
